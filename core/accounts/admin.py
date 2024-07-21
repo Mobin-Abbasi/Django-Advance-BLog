5114c28a04b2a1979e213b36a9c6a6ff33469dc5
Mobin <mobin04dev@gmail.com>
2024-07-21 12:58:56 +0330

    feat: coding admin.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'is_superuser', 'is_active']
+    list_filter = ['email', 'is_superuser', 'is_active']
+    search_fields = ['email']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important date', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important date', {'fields': ('last_login',)}),
+    )

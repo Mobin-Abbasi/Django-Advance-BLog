7fca83dff0a846d7322c1bea20a5ede9c858a965
Mobin <mobin04dev@gmail.com>
2024-07-21 10:57:49 +0330

    feat: create user manager model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,12 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
 
 
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifier
+    for authentication instead of username
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password and extra data
+        """
+        if not email:
+            raise ValueError(_('Please enter an email address'))
+        email = self.normalize_email()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a superuser with the given email and password and extra data
+        """
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_Active', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model for our app
+    """
     email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verified = models.BooleanField(default=False)
@@ -16,6 +53,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email

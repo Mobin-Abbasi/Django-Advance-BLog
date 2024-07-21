31c3da5bdc0fad23ccf3667468acd0181761492e
Mobin <mobin04dev@gmail.com>
2024-07-21 11:21:24 +0330

    fix: fix user manager model bugs

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
         """
         if not email:
             raise ValueError(_('Please enter an email address'))
-        email = self.normalize_email()
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
         """
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_Active', True)
+        extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
